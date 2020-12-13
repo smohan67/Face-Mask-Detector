b55d343697d9e9e8c2b012d3ca3ae033e4abf317
smohan67 <61168189+smohan67@users.noreply.github.com>
2020-12-12 19:08:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -2,28 +2,37 @@ import cv2
 from tensorflow import keras
 from keras.preprocessing.image import load_img , img_to_array
 import numpy as np
-#import create_model
+from pyfirmata import Arduino, util
+#import necessary modules and packages
 
-model = keras.models.load_model('model.h5')
+try:
+	board = Arduino('YOURPORT')#initalize the arduino with the port number
+	servo_pin = board.get_pin('d:PIN_SERVO_IS_CONNECTED_TO:s')#initialize the servo pin
+	servo_pin.write(0)#set the servo motor to 0 degreees
+except:
+	pass
 
-img_width , img_height = 150,150
+model = keras.models.load_model('main/model.h5')#load in the mask detector model
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+img_width , img_height = 150,150#image reshape size
 
-cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')#load in the face detector
+
+cap = cv2.VideoCapture(0)#start capturing the video
 
 img_count_full = 0
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 org = (1,1)
-class_label = ''
+class_label = ''#will be set to mask or no mask
 fontScale = 1
 color = (255,0,0)
 thickness = 2
+#label variables
 
 while True:
 	img_count_full += 1
-	response , color_img = cap.read()
+	response , color_img = cap.read()#captures the current frame
 
 	if response == False:
 		break
@@ -38,32 +47,38 @@ while True:
 
 	gray_img = cv2.cvtColor(color_img,cv2.COLOR_BGR2GRAY)
 
-	faces = face_cascade.detectMultiScale(gray_img, 1.1, 3)
+	faces = face_cascade.detectMultiScale(gray_img, 1.3, 3)#look for faces
 
 	img_count = 0
 	for (x,y,w,h) in faces:
 		org = (x-10,y-10)
 		img_count += 1
 		color_face = color_img[y:y+h,x:x+w]
-		cv2.imwrite('input/face.jpg',color_face)
-		img = load_img('input/face.jpg',target_size=(img_width,img_height))
+		cv2.imwrite('input/fac.jpg',color_face)
+		img = load_img('input/fac.jpg',target_size=(img_width,img_height))
 		img = img_to_array(img)
 		img = np.expand_dims(img,axis=0)
 		prediction = model.predict(img)
-
+	#loop through each face and predict if the face has a mask on or not
 
 		if prediction==1:
 			class_label = "No Mask"
 			color = (0,255,0)
-
+			try:
+				servo_pin.write(90)#change the servo position to 90 degrees
+			except:
+				pass
 		else:
 			class_label = "Mask"
 			color = (255,0,0)
-
-
+			#label stuff
+			try:
+				servo_pin.write(0)#change the servo position to 0 degrees
+			except:
+				pass
 		cv2.rectangle(color_img,(x,y),(x+w,y+h),(0,0,255),3)
 		cv2.putText(color_img, class_label, org, font ,fontScale, color, thickness,cv2.LINE_AA)
-
+		#draw the label around the face
 	cv2.imshow('Face mask detection', color_img)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 		break
